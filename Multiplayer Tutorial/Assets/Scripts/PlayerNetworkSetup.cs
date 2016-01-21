e5e910ac7c6968e12eb888fbafa2ab025e822770
Lutz Kellen <DrWhoCares@users.noreply.github.com>
2016-01-20 21:14:09 -0600

    Tutorial 1 Done

---
+++ Multiplayer Tutorial/Assets/Scripts/PlayerNetworkSetup.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerNetworkSetup : NetworkBehaviour {
+
+	[SerializeField] Camera FPSCharacterCam;
+	[SerializeField] AudioListener FPSCharacterListener;
+
+	void Start () {
+		if (isLocalPlayer) {
+			GameObject.Find ("Scene Camera").SetActive(false);
+			GetComponent<CharacterController> ().enabled = true;
+			GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController> ().enabled = true;
+			FPSCharacterCam.enabled = true;
+			FPSCharacterListener.enabled = true;
+		}
+	}
+}
---
+++ Multiplayer Tutorial/Assets/Scripts/PlayerSyncPosition.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerSyncPosition : NetworkBehaviour {
+
+	[SyncVar] private Vector3 syncPos;
+	[SerializeField] Transform myTransform;
+	[SerializeField] float lerpRate = 15;
+
+	void FixedUpdate () {
+		TransmitPosition();
+		LerpPosition();
+	}
+
+	void LerpPosition() {
+		if (!isLocalPlayer) {
+			myTransform.position = Vector3.Lerp (myTransform.position, syncPos, Time.deltaTime * lerpRate);
+		}
+	}
+
+	[Command]
+	void CmdProvidePositionToServer(Vector3 pos) {
+		syncPos = pos;
+	}
+
+	[ClientCallback]
+	void TransmitPosition() {
+		if(isLocalPlayer) {
+			CmdProvidePositionToServer(myTransform.position);
+		}
+	}
+}
